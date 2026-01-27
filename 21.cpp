20bb897b27ce5c0534b1d79a429ae06f47516aef
MPGP <maksimprevo5@gmail.com>
2026-01-27 15:47:21 +0300

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -5,24 +5,26 @@ using namespace std;
 
 void drawSquare(vector<vector<char>>& field, int x, int y, int size, char symbol)
 {
-    if (size < 2)
+    if (size == 1) {
+        field[y][x] = symbol;
+        return;
+    }
+    if (size < 1)
         return;
 
-    // Верх и низ
     for (int i = 0; i < size; i++)
     {
         field[y][x + i] = symbol;
         field[y + size - 1][x + i] = symbol;
     }
 
-    // Левая и правая стороны
+   
     for (int i = 0; i < size; i++)
     {
         field[y + i][x] = symbol;
         field[y + i][x + size - 1] = symbol;
     }
 
-    // Рекурсивный вызов (внутренний квадрат)
     drawSquare(field, x + 2, y + 2, size - 4, symbol);
 }
 
