14160e22c057e6c799b0c568e884bfee6f171cc5
MPGP <maksimprevo5@gmail.com>
2026-02-03 20:21:23 +0300

    Update 26(INPROGRESS).cpp

--- 26(INPROGRESS).cpp
+++ 26(INPROGRESS).cpp
@@ -1,103 +1,128 @@
 #include <iostream>
 #include <Windows.h>
 using namespace std;
-int n;
-int m;
-string Player1Name = "Игрок1";
-char Player1Figure = 'X';
-string Color1 = "Красный";
-string Color2 = "Синий";
+
+int n, m;
+bool Setting = true;
+
+struct Player {
+    string name;
+    char figure;
+    string color;
+};
+
+Player player1 = { "Игрок1", 'X', "Красный" };
+Player player2 = { "Игрок2", 'O', "Синий" };
+
 void cS() {
 #ifdef _WIN32
-	system("cls");
+    system("cls");
 #else
-	system("clear");
+    system("clear");
 #endif
 }
-void Settings() {
-	setlocale(0, "");
-	cin >> n;
-	cS();
-	switch (n) {
-	case 1:
-		cout << "Введите новый ник: ";
-		cin >> Player1Name;
-		cS();
-		break;
-	case 2:
-		cout << "Введите новую фигуру: ";
-		cin >> Player1Figure;
-		cS();
-		break;
-	case 3:
-		cout << "\033[31m 1 | Красный \033[0m\n" << "\033[34m 2 | Синий \033[0m\n" << "\033[37m 3 | Белый \033[0m\n" << "Выберите желаемый цвет: ";
-		cin >> m;
-		if (m == 1) Color1 = "Красный";
-		else if (m == 2) Color1 = "Синий";
-		else if (m == 3) Color1 = "Белый";
-		cS();
-		break;
-	default:
-		break;
-	}
+
+string ColorCode(const string& color) {
+    if (color == "Красный") return "\033[31m";
+    if (color == "Синий")  return "\033[34m";
+    return "\033[37m";
 }
-void SettingsR() {
-	setlocale(0, "");
-	cout << "+  |  Настройки игрока" << endl << endl << "1  |  Ник     : " << Player1Name << endl << "2  |  Фигура  : " << Player1Figure << endl << "3  |  Цвет    : " << "\033[31m" << Color1 << "\033[0m\n" << endl << endl << ">  | Ввод: ";
-	Settings();
+
+void Settings(Player& p) {
+    cin >> n;
+    cS();
+
+    switch (n) {
+    case 1:
+        cout << "Введите новый ник: ";
+        cin >> p.name;
+        break;
+
+    case 2:
+        cout << "Введите новую фигуру: ";
+        cin >> p.figure;
+        break;
+
+    case 3:
+        cout << "\033[31m1 | Красный\033[0m\n"
+            << "\033[34m2 | Синий\033[0m\n"
+            << "\033[37m3 | Белый\033[0m\n> ";
+        cin >> m;
+        cS();
+        if (m == 1) p.color = "Красный";
+        else if (m == 2) p.color = "Синий";
+        else if (m == 3) p.color = "Белый";
+        break;
+
+    default:
+        Setting = false;
+        break;
+    }
 }
-void SettingsB() {
-	setlocale(0, "");
+
+void SettingsMenu(Player& p) {
+    cout << "+  |  Настройки игрока\n\n"
+        << "1  |  Ник     : " << p.name << "\n"
+        << "2  |  Фигура  : " << p.figure << "\n"
+        << "3  |  Цвет    : "
+        << ColorCode(p.color) << p.color << "\033[0m\n\n"
+        << ">  | Ввод: ";
+
+    Settings(p);
 }
-void SettingsW() {
-	setlocale(0, "");
-}
-void main() {
-	setlocale(0, "");
-	cout << "1 Начать игру" << endl << "2 Статистика" << endl << "3 Настройки" << endl << "4 Выйти" << endl << ">> ";
-	cin >> n;
-	cS();
-	while (true) {
-		switch (n) {
-		case 1:
 
-			break;
-		case 2:
+int main() {
+    setlocale(0, "");
 
-			break;
-		case 3:
-			cout << " +  |  Настройки игрока" << endl << endl << "1  |  Игрок 1" << endl << "2  |  Игрок 2" << endl << endl << ">  | Ввод: ";
-			cin >> n;
-			cS();
-			switch (n) {
-			case 1:
-				while(true){
-					if (Color1 == "Красный") {
-						SettingsR();
-					}
-					else if (Color1 == "Синий") {
-						SettingsB();
-					}
-					else if (Color1 == "Белый") {
-						SettingsW();
-					}
-				}
-				
-			case 2:
+    while (true) {
+        cout << "1 Начать игру\n"
+            << "2 Статистика\n"
+            << "3 Настройки\n"
+            << "4 Выйти\n>> ";
+        cin >> n;
+        cS();
 
-			case 3:
+        switch (n) {
+        case 1:
+            cout << "Игра пока не реализована\n";
+            system("pause");
+            cS();
+            break;
 
-			default:
-				break;
-			}
+        case 2:
+            cout << "Статистика пока не реализована\n";
+            system("pause");
+            cS();
+            break;
 
-		case 4:
-			break;
-		d:
-			cout << "Ошибка!";
-			cin >> n;
-			break;
-		}
-	}
+        case 3:
+            cout << "+  |  Настройки\n\n"
+                << "1  |  Игрок 1\n"
+                << "2  |  Игрок 2\n\n> ";
+            cin >> n;
+            cS();
 
+            if (n == 1) {
+                Setting = true;
+                while (Setting)
+                    SettingsMenu(player1);
+            }
+            else if (n == 2) {
+                Setting = true;
+                while (Setting)
+                    SettingsMenu(player2);
+            }
+            cS();
+            break;
+
+        case 4:
+            return 0;
+
+        default:
+            cout << "Ошибка ввода!\n";
+            system("pause");
+            cS();
+            break;
+        }
+    }
 }
